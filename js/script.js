51aa172032a58c20322474e43cdb446ea26a4105
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-05-11 09:20:19 +0300

    added jquery effects on portfolio and changed the naming of the portfolio icons

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@ $('.main').click(function() {
     $(this).children('.hide').slideToggle(1200);
     $(this).children('.show').toggle(1500)
 });
+$('.card').hover(function () {
+    $(this).children('.card-1').fadeToggle(1000, "linear")
+
+});
+
 });
